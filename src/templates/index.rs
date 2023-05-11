465121c3bec38ed27cc5d84b53a47211b79d63f6
glendc <glen.decauwsemaecker@otainsight.com>
2023-05-11 21:46:00 +0200

    update title

--- src/templates/index.rs
+++ src/templates/index.rs
@@ -50,7 +50,7 @@ fn index_page<G: Html>(cx: Scope) -> View<G> {
 #[engine_only_fn]
 fn head(cx: Scope) -> View<SsrNode> {
     view! { cx,
-        title { "Welcome to Perseus!" }
+        title { "#befossdev" }
         style {
             (r#"
                 body {
